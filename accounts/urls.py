1b895d9d55d3e8411a2bcb6cce5a882c52f31307
Andrew-M-Penzi <mosobera1999@gmail.com>
2022-07-05 23:53:33 +0300

    Setup user signup and registrations and user management

---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+from .views import (
+    base,
+    SignupView,
+    Login
+)
+
+urlpatterns = [
+    path('register/', SignupView.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('base/', base)
+]
---
+++ freelance/urls.py
@@ -0,0 +1,12 @@
+import imp
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from accounts.views import home
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('accounts.urls')),
+    path('', home, name='home')
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
