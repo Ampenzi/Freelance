e77ff1b72a9f2101a0cfed84d0f618c28fc19f70
Andrew-M-Penzi <mosobera1999@gmail.com>
2022-07-08 15:10:08 +0300

    app naming

--- freelance/settings.py
+++ freelance/settings.py
@@ -25,12 +25,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts',
-    'jobs',
+    'accounts.apps.AccountsConfig',
+    'jobs.apps.JobsConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
