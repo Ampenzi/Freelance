8a2db23a5cae2f7ac4dfa20dad4b7499de4affc8
Andrew-Penzi <mosobera1999@gmail.com>
2022-07-10 22:03:23 +0300

    Revert "A few Frontedn changes made"
    This reverts commit 4d210a174a20e5e398e2b4a17ab3ef61e5b41daa.
    
    undoing the commit which had huge mistake
    

---
+++ accounts/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView
+from django.views import View
+
+
+from .forms import SignupForm, LoginForm
+
+
+class SignupView(View):
+    def get(self, request):
+        form = SignupForm()
+        return render(request, 'signup.html', {'form':form})
+    def post(self, request):
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            user.is_active=False
+            user.save()
+            login(request,user)
+            return redirect('login')
+        return render(request, 'signup.html', {'form':form})
+
+
+class Login(LoginView):
+    form_class = LoginForm
+    def form_valid(self, form):
+        self.request.session.set_expiry(0)
+        self.request.session.modified = True
+        return super(Login, self).form_valid(form)
+
+
+def home(request):
+    return render(request, 'index.html')
+
+def base(request):
+    return render(request, 'base.html')
