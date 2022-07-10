4d210a174a20e5e398e2b4a17ab3ef61e5b41daa
Andrew-Penzi <mosobera1999@gmail.com>
2022-07-10 21:53:45 +0300

    A few Frontedn changes made

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from .views import (
-    base,
     SignupView,
-    Login
+    Login,
+    profile,
 )
 
 urlpatterns = [
     path('register/', SignupView.as_view(), name='signup'),
     path('login', Login.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('base/', base)
+    path('profile/', profile, name='profile'),
 ]
